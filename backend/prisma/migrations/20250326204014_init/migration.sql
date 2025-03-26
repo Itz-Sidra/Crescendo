27ecc252622d3379dd3a510e1877bd63a6845dae
Aditya Vasipalli <vasipalliaditya@gmail.com>
2025-03-27 02:26:21 +0530

    do not push to main

--- backend/prisma/migrations/20250326204014_init/migration.sql
+++ backend/prisma/migrations/20250326204014_init/migration.sql
@@ -43,6 +43,19 @@ CREATE TABLE "StudentAssignment" (
 );
 
 -- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "parentName" TEXT NOT NULL,
+    "photo" TEXT NOT NULL,
+    "contactNumber" TEXT NOT NULL,
+    "parentContactNumber" TEXT NOT NULL,
+    "resume" TEXT NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_UserBatches" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL,
